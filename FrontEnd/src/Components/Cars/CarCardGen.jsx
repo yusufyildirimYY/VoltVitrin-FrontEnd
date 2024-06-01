f25d7e4168cb339c102ab3c2b0a0c626f5c98164
DouglassY <teryakiboyzv2@gmail.com>
2024-06-01 17:23:36 +0300

    better

--- FrontEnd/src/Components/Cars/CarCardGen.jsx
+++ FrontEnd/src/Components/Cars/CarCardGen.jsx
@@ -6,29 +6,19 @@ const CarCardGen = ({ Image, Name, Price }) => {
       minimumFractionDigits: 0,
     }).format(price);
   };
+
   return (
-    <div>
-      <div className="relative flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md h-full">
-        <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border ">
-          <img
-            src={Image}
-            alt="car image"
-            className="h-44 max-w-full border-b-2 border-zinc-200"
-          />
-        </div>
-        <div className="p-6 pt-4">
-          <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-            {Name}
-          </h4>
-          <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
-            {formatPrice(Price)}
-          </p>
-        </div>
-        <div className="flex relative bottom-0 items-center justify-between p-6 mt-auto ">
-          <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
-            Details
-          </p>
-        </div>
+    <div className="relative flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border border-b-2 border-zinc-200">
+        <img src={Image} alt="car image" className="h-44 w-full" />
+      </div>
+      <div className="p-6 pt-4">
+        <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+          {Name}
+        </h4>
+        <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
+          {formatPrice(Price)}
+        </p>
       </div>
     </div>
   );
--- FrontEnd/src/Components/Home/CarTabs/CarCards.jsx
+++ FrontEnd/src/Components/Home/CarTabs/CarCards.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import CarCardGen from "../../Cars/CarCardGen";
 const CarCards = ({ Database, activeTab }) => {
   const getSortedData = (
     key,
@@ -13,42 +15,15 @@ const CarCards = ({ Database, activeTab }) => {
       ? getSortedData("Range")
       : getSortedData("Price");
 
-  const formatPrice = (price) => {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 0,
-    }).format(price);
-  };
-
   return (
     <div className="grid grid-rows-2 grid-cols-3 gap-16">
       {sortedData.map((car, index) => (
-        <div
+        <CarCardGen
           key={index}
-          className="relative flex max-w-[24rem] flex-col overflow-hidden rounded-xl bg-white bg-clip-border text-gray-700 shadow-md"
-        >
-          <div className="relative m-0 overflow-hidden text-gray-700 bg-transparent rounded-none shadow-none bg-clip-border border-b-2 border-zinc-200">
-            <img
-              src={car.ModelImage1}
-              alt="car image"
-              className="h-44 w-full"
-            />
-          </div>
-          <div className="p-6 pt-4">
-            <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-              {car.Brand + " " + car.Model}
-            </h4>
-            <p className="block mt-3 font-sans text-xl antialiased font-normal leading-relaxed text-gray-700">
-              {formatPrice(car.Price)}
-            </p>
-          </div>
-          <div className="flex items-center justify-between p-6">
-            <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
-              Details
-            </p>
-          </div>
-        </div>
+          Image={car.ModelImage1}
+          Name={car.Brand + " " + car.Model}
+          Price={car.Price}
+        />
       ))}
     </div>
   );
