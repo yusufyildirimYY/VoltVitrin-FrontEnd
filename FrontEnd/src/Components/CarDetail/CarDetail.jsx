791c4d2d6d5e0d746b4793ff77bb91f292a95cc9
DouglassY <teryakiboyzv2@gmail.com>
2024-06-04 01:01:24 +0300

    Finishing CarDetail page

--- FrontEnd/src/Components/CarDetail/CarDetail.jsx
+++ FrontEnd/src/Components/CarDetail/CarDetail.jsx
@@ -1,5 +1,17 @@
 import { useParams } from "react-router-dom";
+import ImageSlider from "./ImageSlider";
+import { GiSteeringWheel } from "react-icons/gi";
+import { IoMdSpeedometer } from "react-icons/io";
+import { FaRoad } from "react-icons/fa";
+import { GiCarSeat } from "react-icons/gi";
+import { PiSpeedometerFill } from "react-icons/pi";
+import { BsLuggageFill } from "react-icons/bs";
+import { PiPlugChargingBold } from "react-icons/pi";
+import { MdBatteryCharging30 } from "react-icons/md";
+import { PiPlugChargingFill } from "react-icons/pi";
+import { IoMdBatteryCharging } from "react-icons/io";
 
+import "./CarDetail.css";
 const CarDetail = ({ Database }) => {
   const { modelName } = useParams();
   const carDetails = Database.find((car) => car.Model === modelName);
@@ -24,97 +36,90 @@ const CarDetail = ({ Database }) => {
   const image4 = "../../../public" + carDetails.ModelImage4.replace(/^\./, "");
 
   return (
-    <div style={{ height: "969px" }}>
-      <div className="flex h-full">
-        <div className="w-3/4  border-r-2 border-black ">
-          <img
-            className="w-full h-full"
-            src={image1}
-            alt={carDetails.Brand + " " + carDetails.Model}
-          />
+    <div className="flex h-screen z-10 relative">
+      <ImageSlider
+        carDetails={carDetails}
+        image1={image1}
+        image2={image2}
+        image3={image3}
+        image4={image4}
+      />
+      <div className="w-1/4  Details relative z-50 ">
+        <div className="grid text-center mt-10 space-y-5 text-4xl font-semibold">
+          <p className="text-6xl brand"> {carDetails.Brand.toUpperCase()}</p>
+          <p className="whitespace-nowrap model">{carDetails.Model}</p>
         </div>
-        <div className="w-1/4 ">
-          <div className="grid text-center mt-10 space-y-5 text-4xl font-semibold">
-            <p> {carDetails.Brand}</p>
-            <p>{carDetails.Model}</p>
+        <div className="flex flex-col mb-14 text-center items-center text-xl space-y-32 ">
+          <div className="price h-60 text-center items-end pb-10  flex ">
+            <p className="text-3xl font-medium">
+              {formatPrice(carDetails.Price)}
+            </p>
           </div>
-          <div className="flex flex-col text-center items-center text-xl space-y-24 mx-20">
-            <div className="price">
-              <p>{formatPrice(carDetails.Price)}</p>
-            </div>
-            <div className="car flex flex-col">
-              <img src="" alt="" />
-              <div className="flex space-x-20 justify-center">
-                <div className="space-y-2">
-                  <p className="border-b-2 border-black">Seat</p>
-                  <p>{carDetails.Seat}</p>
-                </div>
-                <div className="space-y-2">
-                  <p className="border-b-2 border-black">Drivetrain</p>
-                  <p className="whitespace-nowrap">{carDetails.Drive}</p>
-                </div>
-                <div className="space-y-2">
-                  <p className="whitespace-nowrap border-b-2 border-black ">
-                    Cargo Space
-                  </p>
-                  <p>{carDetails.Cargo + " Liter"}</p>
-                </div>
+          <div className="w-full  ">
+            <div className="containerb ">
+              <div className="space-y-2  flex flex-col items-center div1">
+                <GiSteeringWheel size="3em" />
+                <p className="text-2xl font-semibold ">Drivetrain</p>
+                <p className="whitespace-nowrap ">{carDetails.Drive}</p>
               </div>
-            </div>
-            <div className="way flex flex-col">
-              <img src="" alt="" />
-              <div className="flex space-x-20 justify-center">
-                <div className="space-y-2">
-                  <p className="border-b-2 border-black whitespace-nowrap">
-                    Top Speed
-                  </p>
-                  <p>{carDetails.Top_Speed + " Km/h"}</p>
-                </div>
-                <div className="space-y-2">
-                  <p className="border-b-2 border-black">Range</p>
-                  <p className="whitespace-nowrap">
-                    {carDetails.Range + " Km"}
-                  </p>
-                </div>
-                <div className="space-y-2">
-                  <p className="border-b-2 border-black whitespace-nowrap">
-                    0-100 Acceleration
-                  </p>
-                  <p>{carDetails.Acceleration + " Second"}</p>
-                </div>
+              <div className="space-y-2 flex  flex-col items-center div2">
+                <IoMdSpeedometer size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  Top Speed
+                </p>
+                <p>{carDetails.Top_Speed + " Km/h"}</p>
               </div>
-            </div>
-            <div className="charge flex flex-col space-y-10 ">
-              <img src="" alt="" />
-              <div className="space-y-24">
-                <div className="normalcharge flex space-x-20 text-center items-center justify-center">
-                  <div className="space-y-2">
-                    <p className="border-b-2 border-black whitespace-nowrap">
-                      Charge Power
-                    </p>{" "}
-                    <p>{carDetails.ChargePower}</p>
-                  </div>
-                  <div className="space-y-2">
-                    <p className="border-b-2 border-black whitespace-nowrap">
-                      Charge Speed
-                    </p>
-                    <p>{carDetails.ChargeSpeed}</p>
-                  </div>
-                </div>
-                <div className="fastcharge flex space-x-20 text-center items-cente justify-center">
-                  <div className="space-y-2">
-                    <p className="border-b-2 border-black whitespace-nowrap">
-                      Fast Charge Power
-                    </p>
-                    <p>{carDetails.FastChargePower}</p>
-                  </div>
-                  <div className="space-y-2">
-                    <p className="border-b-2 border-black whitespace-nowrap">
-                      Fast Charge Speed
-                    </p>
-                    <p>{carDetails.FastChargeSpeed}</p>
-                  </div>
-                </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <FaRoad size="3em" />
+                <p className="text-2xl font-semibold">Range</p>
+                <p className="whitespace-nowrap">{carDetails.Range + " Km"}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <GiCarSeat size="3em" />
+                <p className="text-2xl font-semibold ">Seat</p>
+                <p className="">{carDetails.Seat}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <PiSpeedometerFill size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  0-100 Acceleration
+                </p>
+                <p>{carDetails.Acceleration + " Second"}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <BsLuggageFill size="3em" />
+                <p className="whitespace-nowrap text-2xl font-semibold  px-2">
+                  Cargo Space
+                </p>
+                <p className="">{carDetails.Cargo + " Liter"}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <PiPlugChargingBold size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  Charge Power
+                </p>
+                <p>{carDetails.ChargePower}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <MdBatteryCharging30 size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  Charge Speed
+                </p>
+                <p>{carDetails.ChargeSpeed}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <PiPlugChargingFill size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  Fast Charge Power
+                </p>
+                <p>{carDetails.FastChargePower}</p>
+              </div>
+              <div className="space-y-2 flex flex-col items-center">
+                <IoMdBatteryCharging size="3em" />
+                <p className="text-2xl font-semibold whitespace-nowrap">
+                  Fast Charge Speed
+                </p>
+                <p>{carDetails.FastChargeSpeed}</p>
               </div>
             </div>
           </div>
