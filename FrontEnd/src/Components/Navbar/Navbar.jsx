6d428eee4a261b82377f181d84935fd37a016e65
DouglassY <teryakiboyzv2@gmail.com>
2024-06-04 00:56:47 +0300

    restyling navbar looks better

--- FrontEnd/src/Components/Navbar/Navbar.jsx
+++ FrontEnd/src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,50 @@
 import "./Navbar.css";
-import logo from "./voltvitrinLogo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { BsLightningFill } from "react-icons/bs";
 
 function Navbar() {
+  const location = useLocation();
+  const isCarRoute = location.pathname === "/cars";
+  const isCompareRoute = location.pathname === "/compare";
+
+  const navLinkColor =
+    isCarRoute || isCompareRoute ? "text-black" : "text-white";
+
+  const LinkBg =
+    isCarRoute || isCompareRoute ? "hover:bg-gray-200" : "hover:bg-gray-800";
+
+  const Shadow = isCarRoute || isCompareRoute ? "" : "Shadow";
+
+  const svgShadow = isCarRoute || isCompareRoute ? "" : "SvgShadow";
   return (
-    <nav className="border-b-2  ">
-      <div className="max-w-screen-xl flex  items-center  mx-auto p-4">
-        <ul className="flex  p-0   font-medium space-x-52  flex-row mt-0 border-0 text-3xl items-center ">
-          <li>
-            <img src={logo} className="h-12 " alt="VoltVitrin" />
+    <nav className="w-full">
+      <div className="w-full flex items-center justify-center">
+        <ul
+          className={`flex relative p-0 font-medium space-x-10  flex-row border-0  mt-2 w-full items-center text-center justify-center ${navLinkColor}`}
+        >
+          <li
+            className={`flex logo absolute left-5 text-center items-center ${Shadow} cursor-default`}
+          >
+            <BsLightningFill className={`h-10 w-7 ${svgShadow}`} />
+            <p className="text-3xl">VoltVitrin</p>
           </li>
-          <li>
+          <li
+            className={`link  rounded-md transition duration-700 ease-in-out   shadow-black ${Shadow}  ${LinkBg}`}
+          >
             <Link to={"/"} className="block py-2 px-3 rounded lg:p-0">
               Home
             </Link>
           </li>
-          <li className="">
+          <li
+            className={`link rounded-md transition duration-700 ease-in-out ${Shadow} ${LinkBg}`}
+          >
             <Link to={"/cars"} className="block py-2 px-3 lg:p-0">
               Cars
             </Link>
           </li>
-          <li>
+          <li
+            className={`link rounded-md transition duration-700 ease-in-out ${Shadow} ${LinkBg}`}
+          >
             <Link to={"/compare"} className="block py-2 px-3 lg:p-0">
               Compare
             </Link>
