1591d6bf450a5cf1c089550b83fca9935773d539
DouglassY <teryakiboyzv2@gmail.com>
2024-06-05 23:36:57 +0300

    Keep developing , pain

--- FrontEnd/src/Components/Home/CarCanvas/CarCanvas.jsx
+++ FrontEnd/src/Components/Home/CarCanvas/CarCanvas.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Canvas, useLoader, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
 import "./CarCanvas.css";
 import {
   CameraControls,
@@ -11,13 +11,16 @@ import {
   OrbitControls,
   PresentationControls,
   useDepthBuffer,
+  useTexture,
+  PivotControls,
+  Svg,
 } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { AiOutlineFullscreen } from "react-icons/ai";
 import { AiOutlineFullscreenExit } from "react-icons/ai";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/Addons.js";
-import { DEG2RAD } from "three/src/math/MathUtils.js";
+import { GLTFLoader, SVGLoader } from "three/examples/jsm/Addons.js";
+
 const CarCanvas = () => {
   const canvasContainerRef = useRef(null);
   const [isFullScreen, setIsFullScreen] = useState(false);
@@ -56,6 +59,7 @@ const CarCanvas = () => {
     window.addEventListener("beforeunload", cleanup);
     return () => window.removeEventListener("beforeunload", cleanup);
   }, []);
+
   const { DEG2RAD } = THREE.MathUtils;
   return (
     <div className="CanvasContainer" ref={canvasContainerRef}>
@@ -82,34 +86,33 @@ const CarCanvas = () => {
           dpr={[1, 1.5]}
           camera={{
             fov: 90,
-            position: [0, 1, 5.5],
+            position: [0, 5, 13],
           }}
         >
+          {/* <Perf /> */}
           <color attach="background" args={["black"]} />
-          {/* <CameraControls /> */}
-          <PresentationControls rotation={[0.2, 0.05, 0]}>
-            <Light />
-            <mesh
-              receiveShadow
-              rotation={[-Math.PI / 2, 0, 0]}
-              position={[0, -1, -5]}
-            >
-              <circleGeometry args={[20, 74]} />
-              <MeshReflectorMaterial
-                blur={[300, 100]}
-                resolution={2048}
-                mixBlur={1}
-                mixStrength={40}
-                roughness={1}
-                depthScale={1.2}
-                minDepthThreshold={0.7}
-                maxDepthThreshold={1.1}
-                color="#050505"
-                metalness={0.6}
-              />
-            </mesh>
-            <Cards />
-          </PresentationControls>
+          <Light />{" "}
+          <mesh
+            receiveShadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            position={[0, -1, -5]}
+          >
+            <circleGeometry args={[50, 74]} />
+            <MeshReflectorMaterial
+              blur={[300, 100]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={40}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.7}
+              maxDepthThreshold={1.1}
+              color="#050505"
+              metalness={0.6}
+            />
+          </mesh>
+          <OrbitControls enableZoom={false} enablePan={false} />
+          <Cards />
         </Canvas>
       </div>
     </div>
@@ -117,143 +120,106 @@ const CarCanvas = () => {
 };
 
 function Light() {
+  useEffect(() => {
+    spotLight1.current.target = circle.current;
+    spotLight2.current.target = circle.current;
+    spotLight3.current.target = circle.current;
+  }, []);
   const lfloor = useRef();
+  const circle = useRef();
+  const depthBuffer = useDepthBuffer();
+  const spotLight1 = useRef();
+  const spotLight2 = useRef();
+  const spotLight3 = useRef();
   return (
     <>
       <ambientLight intensity={0.015} />
       <directionalLight
         position={[0, 5, 20]}
         color={"white"}
-        intensity={6}
+        intensity={5}
         target={lfloor.current}
       />
       ;
       <mesh ref={lfloor} position={[0, -1, -5]} receiveShadow />;
+      <SpotLight
+        depthBuffer={depthBuffer}
+        ref={spotLight1}
+        position={[0, 20, -15]}
+        target={circle.current}
+        penumbra={0.2}
+        radiusTop={0.4}
+        radiusBottom={25}
+        distance={100}
+        angle={0.45}
+        attenuation={40}
+        anglePower={4}
+        intensity={1}
+        opacity={0.2}
+      />
+      <SpotLight
+        depthBuffer={depthBuffer}
+        ref={spotLight2}
+        position={[15, 20, -10]}
+        target={circle.current}
+        penumbra={0.2}
+        radiusTop={0.4}
+        radiusBottom={20}
+        distance={120}
+        angle={0.45}
+        attenuation={40}
+        anglePower={4}
+        intensity={1}
+        opacity={0.2}
+      />
+      <SpotLight
+        depthBuffer={depthBuffer}
+        ref={spotLight3}
+        position={[-15, 20, -10]}
+        target={circle.current}
+        penumbra={0.2}
+        radiusTop={0.4}
+        radiusBottom={20}
+        distance={120}
+        angle={0.45}
+        attenuation={40}
+        anglePower={4}
+        intensity={1}
+        opacity={0.2}
+      />
+      <mesh position={[0, -2, 0]} ref={circle}>
+        <cylinderGeometry args={[6, 10, 3, 64]} />
+        <meshStandardMaterial color={"black"} />
+      </mesh>
     </>
   );
 }
 function Cards() {
+  const [dragging, setDragging] = useState(false);
+
   const mesh1 = useRef();
-  const mesh2 = useRef();
-  const mesh3 = useRef();
-  const mesh4 = useRef();
-  const spotLight1 = useRef();
-  const spotLight2 = useRef();
-  const spotLight3 = useRef();
-  const spotLight4 = useRef();
 
-  // useHelper(spotLight1, THREE.SpotLightHelper, "teal");
-  useEffect(() => {
-    spotLight1.current.target = mesh1.current;
-    spotLight2.current.target = mesh2.current;
-    spotLight3.current.target = mesh3.current;
-    spotLight4.current.target = mesh4.current;
-  }, []);
-  const depthBuffer = useDepthBuffer();
   const taycan = useLoader(GLTFLoader, "./Models/porshe_taycan/taycan.gltf");
-  const model3 = useLoader(GLTFLoader, "./Models/tesla_model_3/scene.gltf");
-  const ipace = useLoader(GLTFLoader, "./Models/jaguar_i_pace/scene.gltf");
-  const modelx = useLoader(GLTFLoader, "./Models/tesla_model_x/scene.gltf");
 
-  const teslalogo = useLoader(GLTFLoader, "./Models/tesla/scene.gltf");
-  const porschelogo = useLoader(GLTFLoader, "./Models/porsche/scene.gltf");
-  const jaguarlogo = useLoader(GLTFLoader, "./Models/jaguar/scene.gltf");
+  const bind = useDrag(
+    ({ offset: [x, y] }) => {
+      meshRef.current.rotation.x += y * 0.01;
+      meshRef.current.rotation.y += x * 0.01;
+    },
+    {
+      onDragStart: () => setDragging(true),
+      onDragEnd: () => setDragging(false),
+    }
+  );
   return (
     <>
-      <group>
-        <SpotLight
-          depthBuffer={depthBuffer}
-          ref={spotLight1}
-          position={[30, 20, -20]}
-          target={mesh1.current}
-          penumbra={0.2}
-          radiusTop={0.4}
-          radiusBottom={20}
-          distance={150}
-          angle={0.45}
-          attenuation={40}
-          anglePower={4}
-          intensity={1}
-          opacity={0.2}
-        />
-        <mesh ref={mesh1} position={[12, -1, -7]} />
+      <group rotation={[0, 0, 0]}>
         <primitive
-          object={modelx.scene}
-          position={[9.1, -1, -7]}
-          rotation={[0, DEG2RAD * -50, 0]}
-          scale={0.05}
-        />
-      </group>
-      <group>
-        <SpotLight
-          depthBuffer={depthBuffer}
-          ref={spotLight2}
-          position={[25, 20, -30]}
-          target={mesh2.current}
-          penumbra={0.2}
-          radiusTop={0.4}
-          radiusBottom={20}
-          distance={180}
-          angle={0.45}
-          attenuation={40}
-          anglePower={4}
-          intensity={1}
-          opacity={0.2}
-        />
-        <primitive
-          ref={mesh2}
-          position={[8, 0.4, -13.1]}
-          rotation={[0, DEG2RAD * 130, 0]}
-          object={model3.scene}
-          scale={0.02}
-        />
-      </group>
-      <group>
-        <SpotLight
-          depthBuffer={depthBuffer}
-          ref={spotLight3}
-          position={[-20, 20, -25]}
-          target={mesh3.current}
-          penumbra={0.2}
-          radiusTop={0.4}
-          radiusBottom={25}
-          distance={150}
-          angle={0.45}
-          attenuation={40}
-          anglePower={4}
-          intensity={1}
-          opacity={0.2}
-        />
-        <primitive
-          ref={mesh3}
-          position={[-6.5, -1, -13]}
-          rotation={[0, DEG2RAD * 45, 0]}
+          ref={mesh1}
+          position={[0, -0.45, 0]}
+          rotation={[0, 0, 0]}
           object={taycan.scene}
           scale={2}
-        />
-      </group>
-      <group>
-        <SpotLight
-          depthBuffer={depthBuffer}
-          ref={spotLight4}
-          position={[-25, 20, -20]}
-          target={mesh4.current}
-          penumbra={0.2}
-          radiusTop={0.4}
-          radiusBottom={25}
-          distance={180}
-          angle={0.45}
-          attenuation={40}
-          anglePower={4}
-          intensity={1}
-          opacity={0.2}
-        />
-        <mesh ref={mesh4} position={[-12, 0.8, -9]} />
-        <primitive
-          position={[-10.5, 0.8, -8]}
-          rotation={[0, DEG2RAD * 46, 0]}
-          object={ipace.scene}
-          scale={0.6}
         />
       </group>
     </>
