eec53eee1d9c37a6c788d7af0866ee366b355908
DouglassY <teryakiboyzv2@gmail.com>
2024-06-06 18:18:37 +0300

    adding mirror to new image

--- FrontEnd/src/Components/Compare/Compare.jsx
+++ FrontEnd/src/Components/Compare/Compare.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import CompareCards from "./CompareCards";
 
 const Compare = ({ Database }) => {
+  const mirror = "mirror";
   return (
     <div className="compareContainer max-w-screen-xl mx-auto">
       <h1 className="text-4xl font-bold my-16">Compare Cars</h1>
       <div className="w-full  flex space-x-32">
-        <CompareCards Database={Database} />{" "}
+        <CompareCards Database={Database} mirror={mirror} />{" "}
         <CompareCards Database={Database} />
       </div>
     </div>
