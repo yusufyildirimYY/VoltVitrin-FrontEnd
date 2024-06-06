b0dd3e7b5e2adda24f7b7ac8ac8bb9760643a9cf
DouglassY <teryakiboyzv2@gmail.com>
2024-06-06 22:16:20 +0300

    vite config -2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   rollupOptions: {
-    external: ["react", "react-router", "react-router-dom"],
+    external: ["react", "react-router", "react-router-dom", "react-icons/bs"],
     output: {
       globals: {
         react: "React",
